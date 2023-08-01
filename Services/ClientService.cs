477ae093368ec00541bdb41703699348efcb98b2
azizd7000 <azizd7000@gmail.com>
2023-08-01 14:48:34 +0500

    Homework 11 done

--- Services/ClientService.cs
+++ Services/ClientService.cs
@@ -3,25 +3,36 @@ using Crm;
 namespace Crm.Services;
 public sealed class ClientService
 {
+   private List<Client> clientsList = new List<Client>();
    public Client CreateClient(
         string firstName,
         string lastName,
         string middleName,
         short  age,
         string passportNumber,
-        string gender
+        string gender,
+        string phone,
+        string email,
+        string password
 
    ) 
    {
-   return new()
+   var client =  new Client
    {
       FirstName = firstName,
       LastName = lastName,
       MiddleName = middleName,
       Age = age,
       PassportNumber = passportNumber,
-      Gender = gender
+      Gender = gender,
+      Phone = phone,
+      Email = email,
+      Password = password
    }   ;
-   } 
+   clientsList.Add(client);
+   
+   return client;
+   }
+
    
 }
